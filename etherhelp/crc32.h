c86c6a710c2411e13f694839b3249b4698c4794b
cnlohr <lohr85@gmail.com>
2016-03-29 01:40:55 -0400

    forgot to commit the actual crc32s

---
+++ etherhelp/crc32.h
@@ -0,0 +1,11 @@
+#ifndef _CRC32_H
+#define _CRC32_H
+
+#include "eth_config.h"
+
+//Accepts "TABLE_CRC" which takes 512 extra bytes, but is wwaaaayyyy faster
+
+uint32_t crc32(const uint8_t *buf, size_t size);
+
+#endif
+
